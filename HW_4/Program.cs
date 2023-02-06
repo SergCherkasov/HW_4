87944fdd2c6b4bb449eef8acffb3d95d4f3c4635
SergCherkasov <serg.cherkasov@gmail.com>
2023-02-06 13:58:42 +0200

    Connecting UInterface to Main

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace HW_4
+﻿using HW_4.UserInterface;
+
+namespace HW_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            UInterface uInterface= new UInterface();
+            uInterface.Calculate();
         }
     }
 }
